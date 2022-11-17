222a4488452b99cd272d2c666698da727dbdc36d
Alan Chavarin <alanchavarin4@hotmail.com>
2022-11-17 13:09:45 -0800

    history

--- frontend/src/components/assets/EditHistory.jsx
+++ frontend/src/components/assets/EditHistory.jsx
@@ -1,16 +1,21 @@
-import {useEffect} from 'react'
+import {useState} from 'react'
 
 function EditHistory({editHistory}) {
+    const [dropdown, setDropdown] = useState(false)
 
-    useEffect(() => {
-        editHistory?.forEach(test => {
-            console.log(test)
-        })
-    }, [])
+    const onClick = () => {
+        dropdown ? setDropdown(false) : setDropdown(true)
+    }
 
   return (
     <div>
-        {editHistory._id}
+        <button onClick={onClick}>{editHistory._id}</button>
+        {dropdown && ( 
+        <>
+            <div>updated at: {editHistory.updatedAt}</div>
+            <div>change submitted by: {editHistory.editor}</div>
+        </>
+        )}
     </div>
   )
 }
