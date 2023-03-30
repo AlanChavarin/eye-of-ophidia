1c945aea3a022bd46ae5ea718e293974b3818ed9
Alan Chavarin <alanchavarin4@hotmail.com>
2023-03-29 23:19:24 -0700

    fixed frontend directory

--- backend/server.js
+++ backend/server.js
@@ -33,13 +33,13 @@ app.use('/api/eventedithistory', require('./routes/eventEditHistoryRoutes'))
 app.use('/api/events', require('./routes/eventRoutes'))
 app.use('/api/names', require('./routes/nameRoutes'))
 
-// if(process.env.NODE_ENV==='production'){
-//     app.use(express.static('frontend/build'))
+if(process.env.NODE_ENV==='production'){
+    app.use(express.static('frontend/build'))
 
-//     app.get('*', (req, res) => {
-//         res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-//     })
-// }
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve( 'frontend', 'build', 'index.html'))
+    })
+}
 
 // app.get('/api/test/', async (req, res) => {
 
